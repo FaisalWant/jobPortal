8cc4bbba2a2a589717996e0db29fbc1ed87e22b8
FaisalWant <heliconrngr@gmail.com>
2021-03-31 16:38:12 +0530

    Creats Urls for the base.html file-login-logout-register

--- jobs/urls.py
+++ jobs/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from .views import *
 
-
+app_name= "jobs"
 urlpatterns = [
     
     path('', HomeView.as_view(), name='home'),
