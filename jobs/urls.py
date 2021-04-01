c3f16f51c479b95b6f5dedcc0fca5113f8c23130
FaisalWant <heliconrngr@gmail.com>
2021-04-01 17:29:20 +0530

    Posting a new Job

--- jobs/urls.py
+++ jobs/urls.py
@@ -8,4 +8,6 @@ app_name= "jobs"
 urlpatterns = [
     
     path('', HomeView.as_view(), name='home'),
+    path('/create-job/', CreateJobView.as_view(), name='create-job'),
+
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,9 @@
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
 
 # Create your views here.
 from .models import Job, Category
-
-
+from django.contrib.messages.views import SuccessMessageMixin
+from .forms import *
 class HomeView(ListView):
 	template_name='jobs/index.html'
 	context_object_name='jobs'
@@ -15,3 +15,10 @@ class HomeView(ListView):
 		context= super(HomeView,self).get_context_data(**kwargs)
 		context['categories']=Category.objects.all()
 		return context
+
+class CreateJobView(SuccessMessageMixin, CreateView ):
+	model= Job
+	template_name= 'jobs/create-jobs.html'
+	form_class=CreateJobForm
+	success_url = '/job'
+	success_message="Job has been posted"
