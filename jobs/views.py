8040978bd7849f84507d270e0299ad3c169a8097
FaisalWant <heliconrngr@gmail.com>
2021-04-01 18:29:49 +0530

    Creating HTML for Posting a Job

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView, CreateView
-
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .models import Job, Category
 from django.contrib.messages.views import SuccessMessageMixin
@@ -16,9 +17,19 @@ class HomeView(ListView):
 		context['categories']=Category.objects.all()
 		return context
 
+
+
+@method_decorator(login_required(login_url='/users/login'), name='dispatch')
 class CreateJobView(SuccessMessageMixin, CreateView ):
 	model= Job
 	template_name= 'jobs/create-jobs.html'
 	form_class=CreateJobForm
 	success_url = '/job'
 	success_message="Job has been posted"
+
+	def form_valid(self, form):
+		job= form.save(commit=False)
+		job.employer=self.request.user
+		job.save()
+		return super(CreateJobView,self).form_valid(form)
+
